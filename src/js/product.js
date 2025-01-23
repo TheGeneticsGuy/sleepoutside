710406a6c03c012108462a8db88b36915ad84b26
Aaron Topping <aaronjtopping@gmail.com>
2025-01-22 21:11:39 -0700

    Week 3 build Complete

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,5 @@
-import { getLocalStorage, setLocalStorage } from "./utils.mjs";
-import { findProductById } from "./productData.mjs";
+import { getParam } from "./utils.mjs";
+import productDetails from "./productDetails.mjs";
 
-function addProductToCart(product) {
-  let allProductsInCart = getLocalStorage("so-cart");
-
-  if (Array.isArray(allProductsInCart)) {
-    allProductsInCart.push(product);
-  } else {
-    allProductsInCart = [product];
-  }
-
-  setLocalStorage("so-cart", allProductsInCart);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const productId = getParam("product");
+productDetails(productId);
