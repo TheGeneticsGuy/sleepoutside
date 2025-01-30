8d322c27b73bb054361ca933dc163a69d08cff4b
Aaron Topping <aaronjtopping@gmail.com>
2025-01-29 20:54:17 -0700

    Week 4 teams task completed

--- vite.config.js
+++ vite.config.js
@@ -24,10 +24,7 @@ export default defineConfig({
           __dirname,
           "src/product_pages/northface-talus-4.html"
         ),
-        product5: resolve(
-          __dirname,
-          "src/product_pages/index.html"
-        ),
+        product5: resolve(__dirname, "src/product_pages/index.html"),
       },
     },
   },
