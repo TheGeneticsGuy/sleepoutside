f27401d4442fd116397390d0e7d298575680bf7d
Aaron Topping <aaronjtopping@gmail.com>
2025-02-12 21:27:34 -0700

    Week 6 - Team 5 done

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,6 +1,5 @@
 import { findProductById } from "./productData.mjs";
-import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 
 let product = {};
 
@@ -12,24 +11,21 @@ export default async function productDetails(productId) {
     // once the HTML is rendered we can add a listener to Add to Cart button
     document.getElementById("addToCart").addEventListener("click", addToCart);
 }
-
 function addToCart() {
-    let allProductsInCart = getLocalStorage("so-cart");
-
-    if (Array.isArray(allProductsInCart)) {
-        allProductsInCart.push(product);
-    } else {
-        allProductsInCart = [product];
-    };
-
-    setLocalStorage("so-cart", allProductsInCart);
+    let cartContents = getLocalStorage("so-cart");
+    //check to see if there was anything there
+    if (!cartContents) {
+        cartContents = [];
+    }
+    // then add the current product to the list
+    cartContents.push(product);
+    setLocalStorage("so-cart", cartContents);
 }
-
 function renderProductDetails() {
     document.querySelector("#productName").innerText = product.Brand.Name;
     document.querySelector("#productNameWithoutBrand").innerText =
         product.NameWithoutBrand;
-    document.querySelector("#productImage").src = product.Image;
+    document.querySelector("#productImage").src = product.Images.PrimaryLarge;
     document.querySelector("#productImage").alt = product.Name;
     document.querySelector("#productFinalPrice").innerText = product.FinalPrice;
     document.querySelector("#productColorName").innerText =
