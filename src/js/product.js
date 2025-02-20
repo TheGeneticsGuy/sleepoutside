d7e77ab8dd989164b53a4b4217442c7ca7aeeec6
Aaron Topping <aaronjtopping@gmail.com>
2025-02-19 22:50:17 -0700

    Adde netlify Trace in product.js

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,6 @@
 import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
 
+console.log("TEST5");
 const productId = getParam("product");
 productDetails(productId);
