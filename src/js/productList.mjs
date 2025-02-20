95baa46dc9bf863d7ef062e3b129985ea3ffe0c5
Aaron Topping <aaronjtopping@gmail.com>
2025-02-19 21:02:30 -0700

    Week 7 beffore refactor

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,8 +1,7 @@
-import { getData } from "./productData.mjs";
+import { getProductsByCategory } from "./externalServices.mjs";
 import { renderListWithTemplate } from "./utils.mjs";
 
 function productCardTemplate(product) {
-
   return `<li class="product-card">
       <a href="product_pages/index.html?product=${product.Id}">
       <img
@@ -19,7 +18,7 @@ export default async function productList(selector, category) {
   // get the element we will insert the list into from the selector
   const el = document.querySelector(selector);
   // get the list of products
-  const products = await getData(category);
+  const products = await getProductsByCategory(category);
 
   document.querySelector(".title").textContent = category;  // Setting the Title
 
