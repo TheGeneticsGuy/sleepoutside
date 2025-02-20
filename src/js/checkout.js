39efb85524f6fc5fceb28aaca7ae8def1bca8123
Aaron Topping <aaronjtopping@gmail.com>
2025-02-19 23:08:40 -0700

    Rebuilt

--- src/js/checkout.js
+++ src/js/checkout.js
@@ -23,5 +23,5 @@ document.forms["checkout"].addEventListener("submit", (e) => {
 document.querySelector("#checkoutSubmit").addEventListener("click", (e) => {
   e.preventDefault();
 
-  checkoutProcess.checkout(document.forms['checkout']);
+  checkoutProcess.checkout(document.forms["checkout"]);
 });
