da3e4daa602d85957ccc7d65fef4878a79f60bc5
Aaron Topping <aaronjtopping@gmail.com>
2025-02-26 21:13:00 -0700

    Validation Completed - Week 8 - Team 7

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,30 +1,27 @@
 import { findProductById } from "./externalServices.mjs";
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage, alertMessage } from "./utils.mjs";
 
 let product = {};
 
 export default async function productDetails(productId) {
-    console.log("Fetching product:", productId);
     // get the details for the current product. findProductById will return a promise! use await or .then() to process it
     product = await findProductById(productId);
-
     // once we have the product details we can render out the HTML
     renderProductDetails();
     // once the HTML is rendered we can add a listener to Add to Cart button
     document.getElementById("addToCart").addEventListener("click", addToCart);
 }
 function addToCart() {
-    let allProductsInCart = getLocalStorage("so-cart");
-
-    // If it's not an array, wrap it in one
-    if (!Array.isArray(allProductsInCart)) {
-        allProductsInCart = allProductsInCart ? [allProductsInCart] : [];
+    let cartContents = getLocalStorage("so-cart");
+    //check to see if there was anything there
+    if (!cartContents) {
+        cartContents = [];
     }
-
-    allProductsInCart.push(product);
-    setLocalStorage("so-cart", allProductsInCart);
+    // then add the current product to the list
+    cartContents.push(product);
+    setLocalStorage("so-cart", cartContents);
+    alertMessage(`${product.NameWithoutBrand} added to cart!`);
 }
-
 function renderProductDetails() {
     document.querySelector("#productName").innerText = product.Brand.Name;
     document.querySelector("#productNameWithoutBrand").innerText =
